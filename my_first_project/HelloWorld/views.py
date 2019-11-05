d9caf23da1d8224db80a0368cccce5f5639deb98
jatin madaan <jatinmadaaan9882@gmail.com>
2019-11-06 00:10:42 +0530

    template folder added \n made the page dynamic using views file in HelloWorld

--- my_first_project/HelloWorld/views.py
+++ my_first_project/HelloWorld/views.py
@@ -3,5 +3,19 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    response = HttpResponse("Hello World!")
+    developed_by = "Jatin"
+    employees = [
+        "Rishab",
+        "Ankit",
+        "Yash",
+        "Ranpreet",
+        "Sheenky",
+        "Akshay"
+    ]
+
+    context = {
+        "developer": developed_by,
+        "employees": employees
+    }
+    response = render(request, 'index.html', context)
     return response
